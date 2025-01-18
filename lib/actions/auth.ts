8f82a53353cbb4a32b3ec9c65627081863e481ba
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-01-19 00:44:46 +0800

    change url endpoint

--- lib/actions/auth.ts
+++ lib/actions/auth.ts
@@ -70,7 +70,7 @@ export const signUp = async (params: AuthCredentials) => {
     });
 
     await workflowClient.trigger({
-      url: `${config.env.prodApiEndpoint}/api/workflows/onboarding`,
+      url: `${config.env.prodApiEndpoint}/api/workflow/onboarding`,
       body: {
         email,
         fullName,
