b3e354ff15e6db3e125e78f5c6adde8f3d76e4b8
nsoftnhdev <153261745+nsoftnhdev@users.noreply.github.com>
2025-01-18 18:34:03 +0800

    initial commit

---
+++ app/(auth)/sign-in/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import AuthForm from "@/components/AuthForm";
+import { signInSchema } from "@/lib/validation";
+import { signInWithCredentials } from "@/lib/actions/auth";
+
+const Page = () => (
+  <AuthForm
+    type="SIGN_IN"
+    schema={signInSchema}
+    defaultValues={{
+      email: "",
+      password: "",
+    }}
+    onSubmit={signInWithCredentials}
+  />
+);
+export default Page;
